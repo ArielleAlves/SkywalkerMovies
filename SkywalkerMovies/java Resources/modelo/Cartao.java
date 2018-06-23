2b9f9fe14dda0ab96f6bc3855564f3292e695e5e
aluno <aluno@LAB04MC19>
2018-06-22 21:55:36 -0300

    asdadsadsa

--- SkywalkerMovies/java Resources/modelo/Cartao.java
+++ SkywalkerMovies/java Resources/modelo/Cartao.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author aluno
- */
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cartao {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long idCard;
+    private
     
 }
